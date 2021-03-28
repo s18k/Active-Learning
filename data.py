99ff87c48bfb9ec5ee559da1d193ab131dcf44bd
Shreyas Kamath <42207943+s18k@users.noreply.github.com>
2021-03-28 14:09:32 +0530

    Added Image instance related attributes to the Singleton Class

--- data.py
+++ data.py
@@ -8,10 +8,9 @@ class Data:
     @staticmethod
     def getData():
         if Data.__data == None:
-            print("Here")
             Data()
         return Data.__data
-    def __init__(self,counter,X_pool,y_pool,learner,committee,accuracy,X_test,y_test,classlist,queries):
+    def __init__(self,counter,X_pool,y_pool,learner,committee,accuracy,X_test,y_test,classlist,queries,image_data):
         print("Inint called")
         self.counter = counter
         self.X_pool = X_pool
@@ -23,20 +22,22 @@ class Data:
         self.y_test = y_test
         self.classlist = classlist
         self.queries = queries
-        print(type(accuracy))
+        self.image_data = image_data
+        self.image_name_list = []
+        self.image_instance_name = ""
+        self.image_instance = None
         Data.__data = self
 
     def setdata(self,params):
         self.counter = params["counter"]
         self.X_pool = params["X_pool"]
         self.y_pool = params["y_pool"]
-        print(params["accuracy"])
-        print(self.accuracy)
+        self.image_name_list = params["image_name_list"]
+        self.image_instance_name = params["image_instance_name"]
+        self.image_instance = params["image_instance"]
         list = self.accuracy
         list.append(params["accuracy"])
         self.accuracy = list
-        # self.accuracy = list(list(self.accuracy).append(params["accuracy"]))
-        print(self.accuracy)
         Data.__data = self
 
 
